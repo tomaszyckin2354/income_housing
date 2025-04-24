cfa91dd0cb764456de33a4a5941693cc4c4002f3
tomaszyckin2354 <tomaszyckinolan@gmail.com>
2025-04-24 11:49:39 -0400

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
-housing = pd.read_csv('https://raw.githubusercontent.com/iantonios/dsc205/main/CT-affordable-housing-2011-2022.csv')
-income = pd.read_csv('https://raw.githubusercontent.com/iantonios/dsc205/main/CT-towns-income-census-2020.csv')
+df = pd.read_csv('https://raw.githubusercontent.com/iantonios/dsc205/main/CT-affordable-housing-2011-2022.csv')
+df1 = pd.read_csv('https://raw.githubusercontent.com/iantonios/dsc205/main/CT-towns-income-census-2020.csv')
 
 st.title('Affordable Housing in Hartford Compared to Connectict Nolan Tomaszycki & Estela Baka')
 
