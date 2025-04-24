18de9083ece32459c84605d514326ad9172ff6d0
tomaszyckin2354 <tomaszyckinolan@gmail.com>
2025-04-24 11:42:06 -0400

    Create app.py

---
+++ app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+
+
+housing = pd.read_csv('https://raw.githubusercontent.com/iantonios/dsc205/main/CT-affordable-housing-2011-2022.csv')
+income = pd.read_csv('https://raw.githubusercontent.com/iantonios/dsc205/main/CT-towns-income-census-2020.csv')
